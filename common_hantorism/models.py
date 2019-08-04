31a02d3d76cd61329616d6ae98f37ba18e49b360
InWooKwon <jongu7317@gmail.com>
2019-08-04 18:54:51 +0900

    login, model change

--- common_hantorism/models.py
+++ common_hantorism/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 GENDER_CHOICES = [
 ('male','Male'),
@@ -12,21 +13,23 @@ ISHANTOR_CHOICES = [
 
 
 class HantorismUser(models.Model):
-    ID = models.CharField(max_length=20, primary_key=True, unique=True)
-    PW = models.CharField(max_length=20)
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
 
     name = models.CharField(max_length=20)
     studentNum = models.CharField(max_length=10)
     major = models.CharField(max_length=20)
 
-    gender = models.CharField(max_length=8, choices=GENDER_CHOICES)
+    gender = models.CharField(
+        max_length=8,
+        choices=GENDER_CHOICES,
+        null=True)
     email = models.CharField(
         max_length=20)
     isHantor = models.CharField(
         max_length=10,
-        choices=ISHANTOR_CHOICES)
-
-    object=models.Manager()
+        choices=ISHANTOR_CHOICES,
+        null=True)
+    objects = models.Manager()
 
 
 class HantorismPost(models.Model):
--- hantorism_user/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from common_hantorism.models import HantorismUser as Model
-
-class signUpForm(forms.ModelForm):
-    class Meta:
-        model=Model
-        fields=('ID', 'PW', 'name', 'studentNum', 'major', 'gender', 'email', 'isHantor',)
