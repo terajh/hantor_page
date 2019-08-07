cd95cf81970413e14117a539e647776af8b6d985
haejin <bae05030@gmail.com>
2019-08-07 13:54:05 +0900

    error

--- hantorism_library/models.py
+++ hantorism_library/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+# 도서관에서 필요한 모델
+#
+# book
+# -책이름
+# -책 소유자
+# -대여상태
+# -빌린 사람 이름
+# -대여 날짜
+
+# userID = models.ForeignKey(HantorismUser, on_delete = models.CASCADE)
+from common_hantorism.models import HantorismUser
+
+
+class Book(models.Model):
+    book_name = models.CharField(max_length=20)
+    book_rent_state = models.BooleanField(default=False)
+    rent_date = models.DateTimeField(default=timezone.now)
+    book_user = models.ForeignKey(HantorismUser, on_delete=models.CASCADE)
+    book_owner_name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return 'Book name : %s Book owner : %s Book user : %s' % (self.book_name, self.book_owner_name, self.book_user.name)
