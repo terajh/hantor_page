c6d3e3ed22b098e813042de50fbb802be37566e9
haejin <bae05030@gmail.com>
2019-08-19 02:01:20 +0900

    중간과정

--- hantorism_library/views.py
+++ hantorism_library/views.py
@@ -1,8 +1,35 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 from common_hantorism.models import HantorismLibrary as Model
+
+
+class BookReturnViewSet(viewsets.ModelViewSet):
+    def book_return(self, request, book_name, book_owner_name):
+        book = Model.objects.filter(book_name=book_name).get(book_owner_name=book_owner_name)
+        book.rent()
+        context = {'book': book, 'book_name': book_name}
+        return render(request, 'book_return.html', context)
+
+    def book_return_state(self, request, book_name, book_owner_name):
+        book = Model.objects.filter(book_name=book_name)
+
+        context = {'book': book, 'book_name': book_name}
+
+
+class BookRentViewSet(viewsets.ModelViewSet):
+    def book_rent(self, request, book_name, book_owner_name):
+        book = Model.objects.filter(book_name=book_name)
+        context = {'book': book, 'book_name': book_name, 'book_owner_name': book_owner_name}
+        return render(request, 'book_rent.html', context)
+
+    def book_rent_state(self, request, book_name, book_owner_name):
+        book = Model.objects.filter(book_name=book_name).get(book_owner_name=book_owner_name)
+        book.rent()
 
 
 def library(request):
     books = Model.objects.all()
     context = {'books': books}
     return render(request, 'library.html', context)
+
+
