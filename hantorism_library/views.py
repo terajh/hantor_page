0995968f2e548b2d8985a405835415d01ca6e923
haejin <bae05030@gmail.com>
2019-08-23 13:07:58 +0900

    error correction

--- hantorism_library/views.py
+++ hantorism_library/views.py
@@ -1,34 +1,33 @@
 from django.shortcuts import render, redirect
 from common_hantorism.models import HantorismBook, HantorismRentBook, HantorismUser
 from django.utils import timezone
+from django.contrib.auth.decorators import login_required
 
 
+@login_required()
 def book_return(request, book_id):
     book = HantorismBook.objects.get(id=book_id)
     return_books = HantorismRentBook.objects.get(book_id=book_id)
     if request.method == "POST":
         book.book_return()
         return_books.delete()
-        books = HantorismBook.objects.all()
-        context = {'books': books}
         return redirect('/../../library/')
-    elif request.method == "GET":
-        context = {'book': book, 'return_books': return_books}
-        return render(request, 'book_return.html', context)
 
 
+@login_required()
 def book_rent(request, book_id):
     book = HantorismBook.objects.get(id=book_id)
     current_user = HantorismUser.objects.get(user=request.user)
     time = timezone.now()
     if request.method == "POST":
-        book.state = True;
+        book.state = True
         book.save()
-        book = HantorismRentBook(date=time, user=current_user, book=book)
-        book.save()
+        rent_book = HantorismRentBook(date=time, user=current_user, book=book)
+        rent_book.save()
         return redirect('/../../library/')
 
 
+@login_required()
 def library(request):
     books = HantorismBook.objects.all()
     return_books = HantorismRentBook.objects.all()
