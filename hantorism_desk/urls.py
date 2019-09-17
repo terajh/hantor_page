f01803889bc87a3535981a2aec6024cc57b8e08f
haejin <bae05030@gmail.com>
2019-09-18 02:07:00 +0900

    hantorism_dest create

--- hantorism_desk/urls.py
+++ hantorism_desk/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path(r'desk/', views.desk, name='desk')
+    path(r'desk/', views.dodesk, name='dodesk'),
+    path(r'desk_list/',views.desk_list, name='desk_list')
 ]
