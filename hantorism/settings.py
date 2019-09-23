8d2f8ffc97782cf659c9d103b8aa852a31d33fa6
jaypae95 <jaypae95@hbsmith.io>
2019-09-23 22:44:59 +0900

    nginx static location

--- hantorism/settings.py
+++ hantorism/settings.py
@@ -144,5 +144,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'hantorism/static')]
 LOGIN_URL = '/sign_in'
