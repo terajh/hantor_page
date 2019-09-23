a604eff0de6490f481b59d0c27f524b3ea853a55
jaypae95 <jaypae95@hbsmith.io>
2019-09-24 02:21:00 +0900

    gg

--- hantorism/settings.py
+++ hantorism/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-
+from hantorism.settings_local import SETTINGS_SECRET_KEY
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'um#-bz5a@220+y%m1nn+k__p$!g4^#0fogkpsil#nj81^*ankj'
+SECRET_KEY = SETTINGS_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
